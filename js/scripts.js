1cc236c6d9f42fc92bfd8fa29dc9a2bc40d5644b
[brycen] <[brycenbartolome@gmail.com]>
2020-08-11 13:54:05 -0700

    add blue boxes definition and answer
    co-authored-by: Drew Lockhart <drewmlockhart@gmail.com

---
+++ js/scripts.js
@@ -0,0 +1,54 @@
+$(document).ready(function() {
+  $("#javascript").click(function() {
+    $(".definition-java").toggle();
+    $(".answer-java").toggle();
+  });
+
+  $("#operators").click(function() {
+    $(".definition-operators").toggle();
+    $(".answer-operators").toggle();
+  });
+
+  $("#").click(function() {
+    $(".definition-").toggle();
+    $(".answer-").toggle();
+  });
+
+  $("#").click(function() {
+    $(".definition-").toggle();
+    $(".answer-").toggle();
+  });
+
+  $("#").click(function() {
+    $(".definition-").toggle();
+    $(".answer-").toggle();
+  });
+
+  $("#").click(function() {
+    $(".definition-").toggle();
+    $(".answer-").toggle();
+  });
+
+  $("#").click(function() {
+    $(".definition-").toggle();
+    $(".answer-").toggle();
+  });
+
+  $("#").click(function() {
+    $(".definition-").toggle();
+    $(".answer-").toggle();
+  });
+
+  $("#").click(function() {
+    $(".definition-").toggle();
+    $(".answer-").toggle();
+  });
+
+  $("#").click(function() {
+    $(".definition-").toggle();
+    $(".answer-").toggle();
+  });
+
+
+
+});
