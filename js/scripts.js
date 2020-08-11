cef533882fe529617c8a164afd80a55a70d1adf0
[brycen] <[brycenbartolome@gmail.com]>
2020-08-11 14:16:06 -0700

    add all answers
    co-authored-by: Drew Lockhart <drewmlockhart@gmail.com

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,67 @@
 $(document).ready(function() {
-  $("#javascript").click(function() {
-    $(".definition-java").toggle();
-    $(".answer-java").toggle();
+  $("#Java").click(function() {
+    $(".definition-Java").toggle();
+    $(".answer-Java").toggle();
   });
 
-  $("#operators").click(function() {
-    $(".definition-operators").toggle();
-    $(".answer-operators").toggle();
+  $("#Operators").click(function() {
+    $(".definition-Operators").toggle();
+    $(".answer-Operators").toggle();
+  });
+
+  $("#Variables").click(function() {
+    $(".definition-Variables").toggle();
+    $(".answer-Variables").toggle();
+  });
+
+  $("#vnc").click(function() {
+    $(".definition-vnc").toggle();
+    $(".answer-vnc").toggle();
+  });
+
+  $("#Functions").click(function() {
+    $(".definition-Functions").toggle();
+    $(".answer-Functions").toggle();
+  });
+
+  $("#Methods").click(function() {
+    $(".definition-Methods").toggle();
+    $(".answer-Methods").toggle();
+  });
+
+  $("#Arguments").click(function() {
+    $(".definition-Arguments").toggle();
+    $(".answer-Arguments").toggle();
+  });
+
+  $("#Parameters").click(function() {
+    $(".definition-Parameters").toggle();
+    $(".answer-Parameters").toggle();
+  });
+
+  $("#Return").click(function() {
+    $(".definition-Return").toggle();
+    $(".answer-Return").toggle();
+  });
+
+  $("#Chaining-methods").click(function() {
+    $(".definition-Chaining-methods").toggle();
+    $(".answer-Chaining-methods").toggle();
+  });
+
+  $("#Strings").click(function() {
+    $(".definition-Strings").toggle();
+    $(".answer-Strings").toggle();
+  });
+
+  $("#Booleans").click(function() {
+    $(".definition-Booleans").toggle();
+    $(".answer-Booleans").toggle();
+  });
+
+  $("#Undefined").click(function() {
+    $(".definition-Undefined").toggle();
+    $(".answer-Undefined").toggle();
   });
 
   $("#").click(function() {
@@ -14,9 +69,34 @@ $(document).ready(function() {
     $(".answer-").toggle();
   });
 
-  $("#").click(function() {
-    $(".definition-").toggle();
-    $(".answer-").toggle();
+  $("#NaN").click(function() {
+    $(".definition-NaN").toggle();
+    $(".answer-NaN").toggle();
+  });
+
+  $("#Null").click(function() {
+    $(".definition-Null").toggle();
+    $(".answer-Null").toggle();
+  });
+
+  $("#Alert").click(function() {
+    $(".definition-Alert").toggle();
+    $(".answer-Alert").toggle();
+  });
+
+  $("#Comments").click(function() {
+    $(".definition-Comments").toggle();
+    $(".answer-Comments").toggle();
+  });
+
+  $("#jQuery").click(function() {
+    $(".definition-jQuery-").toggle();
+    $(".answer-jQuery").toggle();
+  });
+
+  $("#Attributes").click(function() {
+    $(".definition-Attributes").toggle();
+    $(".answer-Attributes").toggle();
   });
 
   $("#").click(function() {
